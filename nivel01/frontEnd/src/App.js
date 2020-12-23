7c5e6762c1d28776f7af0a8cbee8374eb7b96bbb
José Augusto <joseaugusto0408@gmail.com>
2020-12-23 06:02:31 -0300

    Importing css and image

--- nivel01/frontEnd/src/App.js
+++ nivel01/frontEnd/src/App.js
@@ -1,24 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
+import "./App.css";
+import imageBackground from "./assets/logogtech.png";
 import Header from "./components/Header";
 
-function App(){
 
-  const [projects, setProjects] = useState(['Desenvolvimento com React JS', 'Desenvolvimento com PHP'])
+function App() {
+    const [projects, setProjects] = useState([
+        "Desenvolvimento com React JS",
+        "Desenvolvimento com PHP",
+    ]);
 
-  function handleAddProject() {
-    setProjects([...projects, `Novo projeto ${Date.now()}`])
-    
-  }
-  return (
-      <>
-          <Header title="Curso de React JS" />
-          <ul>
-            {projects.map(project => <li key={project}>{project}</li>)}
-          </ul>
-          <button type='button' onClick={handleAddProject}>Adicionar novo projeto</button>
-      </>
-  );
+    function handleAddProject() {
+        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    }
+    return (
+        <>
+            <Header title="Curso de React JS" />
+            <ul>
+                {projects.map((project) => (
+                    <li key={project}>{project}</li>
+                ))}
+            </ul>
+            <button type="button" onClick={handleAddProject}>
+                Adicionar novo projeto
+            </button>{" "}
+            <br />
+            <img src={imageBackground} alt="logomarca" width={200}/>
+        </>
+    );
 }
 
 export default App;
