cd1e6f364f832aa08345a951bcf900d717e0933e
José Augusto <joseaugusto0408@gmail.com>
2020-12-23 07:27:18 -0300

    Registering and listing api

--- nivel01/frontEnd/src/App.js
+++ nivel01/frontEnd/src/App.js
@@ -1,32 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
+import api from "./services/api";
 import "./App.css";
+
 import imageBackground from "./assets/logogtech.png";
 import Header from "./components/Header";
 
-
 function App() {
-    const [projects, setProjects] = useState([
-        "Desenvolvimento com React JS",
-        "Desenvolvimento com PHP",
-    ]);
+    const [projects, setProjects] = useState([]);
 
-    function handleAddProject() {
-        setProjects([...projects, `Novo projeto ${Date.now()}`]);
+    useEffect(() => {
+        api.get("projects").then((response) => {
+            setProjects(response.data);
+        });
+    }, []);
+
+    async function handleAddProject() {
+        const response = await api.post("projects", {
+            title: `Novo Projeto ${Date.now()}`,
+            owner: "José Augusto",
+        });
+        const project = response.data;
+        setProjects([...projects, project]);
     }
     return (
         <>
-            <Header title="Curso de React JS" />
+            <Header title="Curso Rocketseat" />
             <ul>
                 {projects.map((project) => (
-                    <li key={project}>{project}</li>
+                    <li key={project.id}>{project.title}</li>
                 ))}
             </ul>
             <button type="button" onClick={handleAddProject}>
                 Adicionar novo projeto
             </button>{" "}
             <br />
-            <img src={imageBackground} alt="logomarca" width={200}/>
+            <img src={imageBackground} alt="logomarca" width={200} />
         </>
     );
 }
