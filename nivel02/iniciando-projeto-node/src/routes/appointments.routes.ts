d435523bc9af66b24cdee07374c897ab9856de83
José Augusto <joseaugusto0408@gmail.com>
2020-12-27 18:17:42 -0300

    chore: add typeorm pg

---
+++ nivel02/iniciando-projeto-node/src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from "express";
+import { parseISO } from "date-fns";
+import AppointmentsRepository from "../repositories/AppointmentsRepository";
+import CreateAppointmentService from '../services/CreateAppointmentService'
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get("/", (request, response) => {
+  const appointment = appointmentsRepository.all();
+
+  return response.status(200).json(appointment);
+});
+
+appointmentsRouter.post("/", (request, response) => {
+  
+  try{
+    const { provider, date } = request.body;
+  
+  const parsedDate = parseISO(date);
+
+  const createAppointmentService = new CreateAppointmentService(appointmentsRepository);
+
+  const appointment = createAppointmentService.execute({provider, date:parsedDate});
+  
+  return response.json(appointment);
+  } catch(err){
+    return response.status(400).json({error: err.message})
+  }
+});
+
+export default appointmentsRouter;
