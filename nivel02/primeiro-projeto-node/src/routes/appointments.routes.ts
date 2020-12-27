682551119114a29e27e67576f6bd751509e3426d
José Augusto <joseaugusto0408@gmail.com>
2020-12-27 10:25:16 -0300

    chore: add list all appointments

--- nivel02/primeiro-projeto-node/src/routes/appointments.routes.ts
+++ nivel02/primeiro-projeto-node/src/routes/appointments.routes.ts
@@ -1,9 +1,15 @@
-import { Router } from "express";
+import { response, Router } from "express";
 import { startOfHour, parseISO } from "date-fns";
 import AppointmentsRepository from "../repositories/AppointmentsRepository";
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get("/", (request, response) => {
+  const appointment = appointmentsRepository.all();
+
+  return response.status(200).json(appointment);
+});
 
 appointmentsRouter.post("/", (request, response) => {
   const { provider, date } = request.body;
