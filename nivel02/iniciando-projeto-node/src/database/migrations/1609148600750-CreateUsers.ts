9fa210ccc6c2935d29ec1db6555ad62ada75302a
José Augusto <joseaugusto0408@gmail.com>
2020-12-28 06:57:45 -0300

    chore: add table users

--- nivel02/iniciando-projeto-node/src/database/migrations/1609148600750-CreateUsers.ts
+++ nivel02/iniciando-projeto-node/src/database/migrations/1609148600750-CreateUsers.ts
@@ -1,9 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from "typeorm";
-export class CreateAppointments1609107645749 implements MigrationInterface {
+
+export class CreateUsers1609148600750 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "appointments",
+        name: "users",
         columns: [
           {
             name: "id",
@@ -13,14 +14,17 @@ export class CreateAppointments1609107645749 implements MigrationInterface {
             default: "uuid_generate_v4()",
           },
           {
-            name: "provider",
+            name: "name",
             type: "varchar",
-            isNullable: false,
           },
           {
-            name: "date",
-            type: "timestamp with time zone",
-            isNullable: false,
+            name: "email",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "password",
+            type: "varchar",
           },
           {
             name: "created_at",
@@ -38,6 +42,6 @@ export class CreateAppointments1609107645749 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("appointments");
+    await queryRunner.dropTable("users");
   }
 }
--- nivel02/iniciando-projeto-node/src/models/Appointment.ts
+++ nivel02/iniciando-projeto-node/src/models/Appointment.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn, CreateDateColumn,
+  UpdateDateColumn, } from "typeorm";
 
 @Entity('appointments')
 class Appointment {
@@ -10,5 +11,11 @@ class Appointment {
 
   @Column('timestamp with time zone')
   date: Date;
+
+   @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
 }
 export default Appointment;
