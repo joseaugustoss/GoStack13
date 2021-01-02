ff2b04e3d942f04da2ed9f51febcc8062b1b6940
José Augusto <joseaugusto0408@gmail.com>
2021-01-01 22:27:34 -0300

    chore: express-async-errors, refact deal with error

--- nivel02/iniciando-projeto-node/src/routes/sessions.routes.ts
+++ nivel02/iniciando-projeto-node/src/routes/sessions.routes.ts
@@ -4,27 +4,23 @@ import AuthenticateUserService from "../services/AuthenticateUserService";
 const sessionsRoutes = Router();
 
 sessionsRoutes.post("/", async (request, response) => {
-  try {
-    const { email, password } = request.body;
+  const { email, password } = request.body;
 
-    const authenticateUser = new AuthenticateUserService();
+  const authenticateUser = new AuthenticateUserService();
 
-    const { user, token } = await authenticateUser.execute({
-      email,
-      password,
-    });
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
 
-    const userWithoutPassword = {
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      created_up: user.created_at,
-      updated_at: user.updated_at,
-    };
-    return response.status(200).json({ user: userWithoutPassword, token });
-  } catch (err) {
-    return response.status(err.statusCode).json({ error: err.message });
-  }
+  const userWithoutPassword = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    created_up: user.created_at,
+    updated_at: user.updated_at,
+  };
+  return response.status(200).json({ user: userWithoutPassword, token });
 });
 
 export default sessionsRoutes;
