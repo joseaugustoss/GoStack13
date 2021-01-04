69807dcc153f447420d55578502d1e9224b18ef4
José Augusto <joseaugusto0408@gmail.com>
2021-01-04 11:32:11 -0300

    feat: navigate routes

--- nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ nivel03/05-primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, FormEvent } from "react";
 import api from "../../services/api";
-
 import { FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router-dom";
+
 import logoImg from "../../assets/logo.svg";
 import { Title, Form, Error, Repositories } from "./style";
 
@@ -73,7 +74,7 @@ const Dashboard: React.FC = () => {
             {inputError && <Error>{inputError}</Error>}
             <Repositories>
                 {repositories.map((repository) => (
-                    <a key={repository.full_name} href="teste">
+                    <Link key={repository.full_name} to={`/repositories/${repository.full_name}`}>
                         <img
                             src={repository.owner.avatar_url}
                             alt={repository.owner.login}
@@ -83,7 +84,7 @@ const Dashboard: React.FC = () => {
                             <p>{repository.description}</p>
                         </div>
                         <FiChevronRight size={20} />
-                    </a>
+                    </Link>
                 ))}
             </Repositories>
         </>
