6cb3a0197d8fd132b771ed11d82ec37086b7547f
José Augusto <joseaugusto0408@gmail.com>
2021-01-05 09:18:23 -0300

    feat: list API issues

--- nivel03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
+++ nivel03/05-primeiro-projeto-react/src/pages/Repository/index.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useRouteMatch, Link } from "react-router-dom";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import api from "../../services/api";
 
 import logoImg from "../../assets/logo.svg";
 import { Header, RepositoryInfo, Issues } from "./style";
@@ -8,9 +9,43 @@ import { Header, RepositoryInfo, Issues } from "./style";
 interface RepositoryParams {
     repository: string;
 }
+interface Repository {
+    full_name: string;
+    description: string;
+    stargazers_count: number;
+    forks_count: number;
+    open_issues_count: number;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
+
+interface Issue {
+    id: number;
+    title: string;
+    html_url: string;
+    user: {
+        login: string;
+    };
+}
 
 const Repository: React.FC = () => {
+    const [repository, setRepository] = useState<Repository | null>(null);
+    const [issues, setIssues] = useState<Issue[]>([]);
+
     const { params } = useRouteMatch<RepositoryParams>();
+
+    useEffect(() => {
+        api.get(`repos/${params.repository}`).then((response) => {
+            setRepository(response.data);
+        });
+    }, [params.repository]);
+    useEffect(() => {
+        api.get(`repos/${params.repository}/issues`).then((response) => {
+            setIssues(response.data);
+        });
+    }, [params.repository]);
     return (
         <>
             <Header>
@@ -20,42 +55,46 @@ const Repository: React.FC = () => {
                     Voltar
                 </Link>
             </Header>
-            <RepositoryInfo>
-                <header>
-                    <img
-                        src="https://avatars0.githubusercontent.com/u/28929274?v=4"
-                        alt="rocketseat"
-                    />
-                    <div>
-                        <strong>rocketseat/unform</strong>
-                        <p>Descrição do repositório</p>
-                    </div>
-                </header>
+            {repository && (
+                <RepositoryInfo>
+                    <header>
+                        <img
+                            src={repository.owner.avatar_url}
+                            alt={repository.owner.login}
+                        />
+                        <div>
+                            <strong>{repository.full_name}</strong>
+                            <p>{repository.description}</p>
+                        </div>
+                    </header>
 
-                <ul>
-                    <li>
-                        <strong>1800</strong>
-                        <span>Starts</span>
-                    </li>
-                    <li>
-                        <strong>48</strong>
-                        <span>Forks</span>
-                    </li>
-                    <li>
-                        <strong>67</strong>
-                        <span>Issues Abertas</span>
-                    </li>
-                </ul>
-            </RepositoryInfo>
+                    <ul>
+                        <li>
+                            <strong>{repository.stargazers_count}</strong>
+                            <span>Starts</span>
+                        </li>
+                        <li>
+                            <strong>{repository.forks_count}</strong>
+                            <span>Forks</span>
+                        </li>
+                        <li>
+                            <strong>{repository.open_issues_count}</strong>
+                            <span>Issues Abertas</span>
+                        </li>
+                    </ul>
+                </RepositoryInfo>
+            )}
 
             <Issues>
-                <Link to="ahuahauahau">
-                    <div>
-                        <strong>uhauahuahau</strong>
-                        <p>aiaahaahuahua</p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </Link>
+                {issues.map((issue) => (
+                    <a key={issue.id} href={issue.html_url}>
+                        <div>
+                            <strong>{issue.title}</strong>
+                            <p>{issue.user.login}</p>
+                        </div>
+                        <FiChevronRight size={20} />
+                    </a>
+                ))}
             </Issues>
         </>
     );
