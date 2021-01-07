3c57987faf81d1af1b0d6f0e131f4096aa380e45
José Augusto <joseaugusto0408@gmail.com>
2021-01-07 12:17:09 -0300

    feat: isolate component

--- nivel03/06-gobarber/src/pages/SignIn/index.tsx
+++ nivel03/06-gobarber/src/pages/SignIn/index.tsx
@@ -1,35 +1,43 @@
 import React from "react";
-import { FiLogIn } from "react-icons/fi";
+import { FiLogIn, FiMail, FiLock } from "react-icons/fi";
 
 import logoImg from "../../assets/logo.svg";
 import { Container, Content, Background } from "./styles";
 
+import Input from "../../components/Input/index";
+import Button from "../../components/Button/index";
+
 const SignIn: React.FC = () => {
+    return (
+        <>
+            <Container>
+                <Content>
+                    <img src={logoImg} alt="Gobarber" />
+                    <form>
+                        <h1>Faça seu login</h1>
 
-  return (
-      <>
-          <Container>
-              <Content>
-                  <img src={logoImg} alt="Gobarber" />
-                  <form>
-                      <h1>Faça seu login</h1>
+                        <Input 
+                        icon={FiMail}
+                        name="email"
+                        placeholder="Email" />
+                        <Input 
+                        icon={FiLock}
+                        name="password"
+                        type="password" placeholder="senha" />
 
-                      <input placeholder="Email" />
-                      <input type='password' placeholder="senha" />
+                        <Button type="submit">Entrar</Button>
+                        <a href="forgot">Esqueci minha senha</a>
+                    </form>
 
-                      <button type='submit'>Entrar</button>
-                      <a href="forgot">Esqueci minha senha</a>
-                  </form>
-
-                  <a href="login">
-                    <FiLogIn />
-                    Criar conta
-                  </a>
-              </Content>
-              <Background/>
-          </Container>
-      </>
-  );
-}
+                    <a href="login">
+                        <FiLogIn />
+                        Criar conta
+                    </a>
+                </Content>
+                <Background />
+            </Container>
+        </>
+    );
+};
 
 export default SignIn;
